2b4fbdb4ee93230399ea94c261bfed01c4541894
mwas <francismwangi152@gmail.com>
2019-11-06 16:53:03 +0300

    stop using valid credentials

--- provider.tf
+++ provider.tf
@@ -6,6 +6,8 @@ provider "aws" {
   region  = "eu-west-1"
   version = "~> 2.31"
   alias = "custom_provider"
+  skip_credentials_validation = true
+  skip_requesting_account_id = true
   endpoints {
     dynamodb = var.dynamodb_endpont
   }
